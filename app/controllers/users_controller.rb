f4c1aeace9757969adb4472faad7c79e96d90ea3
Brett D. McLaughlin <brett.m@me.com>
2018-10-12 12:05:38 -0500

    Finish up user actions, including admin support.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   
   def show
     @user = User.find(params[:id])
@@ -6,6 +9,10 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def create
@@ -17,6 +24,39 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "The requested user was deleted."
+    redirect_to users_url
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "You must login before accessing this system."
+      redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 
   private
 
@@ -24,5 +64,9 @@ class UsersController < ApplicationController
       params.require(:user).permit(:first_name, :last_name,
                                    :email, :password,
                                    :password_confirmation)
+    end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
